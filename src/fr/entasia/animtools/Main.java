984120978ec6e1a548910e9322e3ca699e9e1b83
iTrooz_ <team.mortelgames@gmail.com>
2020-08-07 21:21:33 +0200

    fix

--- src/fr/entasia/animtools/Main.java
+++ src/fr/entasia/animtools/Main.java
@@ -7,6 +7,7 @@ import fr.entasia.animtools.listeners.Tools;
 import fr.entasia.animtools.utils.SBTask;
 import fr.entasia.animtools.utils.Utils;
 import fr.entasia.animtools.utils.enums.EventFlags;
+import fr.entasia.errors.EntasiaException;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,6 +26,7 @@ public class Main extends JavaPlugin {
 			saveDefaultConfig();
 
 			Utils.animWorld = Bukkit.getWorld(getConfig().getString("world"));
+			if(Utils.animWorld==null)throw new EntasiaException("World "+getConfig().getString("world")+" Not found");
 			spawn = getConfig().getConfigurationSection("spawn");
 			Utils.eventSpawn = Utils.animWorld.getSpawnLocation();
 			Utils.eventSpawn.setX(spawn.getInt("x")+0.5);
